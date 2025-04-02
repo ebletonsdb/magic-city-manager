8ac6aecac15f91b90581cee3810356e40f05e50e
Ebleton <145159105+ebletonsdb@users.noreply.github.com>
2025-04-02 14:02:44 -0400

    Update print_reservations_crud1.php

--- ajax/print_reservations_crud1.php
+++ ajax/print_reservations_crud1.php
@@ -2,8 +2,7 @@
 
 require('../inc/db_config.php');
 require('../inc/essentials.php');
-require_once __DIR__ . '/../inc/tcpdf/tcpdf.php';
-
+require_once __DIR__ . '../inc/TCPDF/tcpdf.php';
 adminLogin();
 
 if (isset($_POST['print_reservations'])) {
