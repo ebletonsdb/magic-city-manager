c37b9a3d04f10edabd4f336e82cd156c2b523a27
Ebleton <145159105+ebletonsdb@users.noreply.github.com>
2025-04-02 14:20:38 -0400

    Update print_rapports_crud1.php

--- ajax/print_rapports_crud1.php
+++ ajax/print_rapports_crud1.php
@@ -1,7 +1,7 @@
 <?php
     require('../inc/db_config.php');
     require('../inc/essentials.php');
-    require('./inc/TCPDF/tcpdf.php');
+    require('TCPDF/tcpdf.php');
     adminLogin();
 
     if (isset($_POST['print_rapports'])) {
